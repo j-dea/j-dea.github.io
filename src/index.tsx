95f40cade8825c23b939c1da140cb5b2f7a78010
Cattamoo <ouob77@kakao,com>
2023-01-11 13:38:52 +0900

    log 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {printLog} from "./util/log";
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -14,3 +15,4 @@ root.render(
 );
 
 reportWebVitals();
+printLog();
---
+++ src/util/log.ts
@@ -0,0 +1,28 @@
+export function printLog(): void {
+	const style = {
+		noStyle: '',
+		title: {
+			all: 'font-size: 2rem; font-weight: bold; color: #ffd737;',
+			j: 'font-size: 2rem; font-weight: bold; color: #b2cadd; background-color: #ffffdb;',
+			d: 'font-size: 2rem; font-weight: bold; color: #ddb2ca; background-color: #ffffdb;',
+			e: 'font-size: 2rem; font-weight: bold; color: #cab2dd; background-color: #ffffdb;',
+			a: 'font-size: 2rem; font-weight: bold; color: #caddb2; background-color: #ffffdb;',
+		},
+		projectName: 'color: white; background-color: #565492;',
+		link: 'color: #dddddd;'
+	}
+
+	const text = `
+%c J%cD%cE%cA %c  
+%c  Github Pages  
+%chttps://github.com/j-dea/j-dea.github.io/
+`;
+
+	const styles = [
+		style.title.j, style.title.d, style.title.e, style.title.a, style.title.all,
+		style.projectName,
+		style.link,
+	];
+
+	console.log(text, ...styles);
+}
